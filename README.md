71207d2facfffd96cfcb5963353df612d60021b7
zschaudhry <116850380+zschaudhry@users.noreply.github.com>
2023-12-08 23:45:52 +0000

    Refactor code to use Azure AI services and handle file uploads

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # azureAi
+# Azure AI
+
+This project is focused on leveraging Azure AI services to solve complex problems.
+
+## Description
+
+This project uses various Azure AI services to build intelligent solutions. It includes examples of how to use Azure Cognitive Services, Azure Machine Learning, and other Azure AI services.
+
+## Installation
+
+To get this project up and running, follow these steps:
+
+```bash
+git clone <your-repo>
+cd <your-repo>
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Importing necessary modules
 import { OpenAIClient, AzureKeyCredential } from "@azure/openai";
 import express from 'express';
 import dotenv from 'dotenv';
@@ -10,8 +11,10 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const pdfParse = require('pdf-parse');
 
+// Loading environment variables
 dotenv.config();
 
+// Setting up necessary constants
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const upload = multer({ dest: 'uploads' });
@@ -21,9 +24,11 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+// Setting up the port
 const port = process.env.PORT || 3000;
 
- function filterText(text) {
+// Function to filter text
+function filterText(text) {
   const words = text.split(" ");
   console.log(words.length);
   const filteredWords = stopword.removeStopwords(words, stopwords);
@@ -34,31 +39,36 @@ const port = process.env.PORT || 3000;
   console.log(finaltxt.length)
   return finaltxt;
 }
+
 let filteredText="";
+
+// Route to handle file upload
 app.post('/upload', upload.single('file'), (req, res) => {
   const dataBuffer = fs.readFileSync(req.file.path);
+  pdfParse(dataBuffer).then(data => {
+    filteredText = filterText(data.text.toLowerCase());
+    res.send(filteredText);
 
-   pdfParse(dataBuffer).then(data => {
-     filteredText = filterText(data.text.toLowerCase());
-      res.send(filteredText);
-
-      fs.unlinkSync(req.file.path, err => {
-          if (err) console.error("Error deleting the file:", err);
-          else console.log("File read successfully and then deleted successfully");
-      });
+    // Delete the file after reading
+    fs.unlinkSync(req.file.path, err => {
+      if (err) console.error("Error deleting the file:", err);
+      else console.log("File read successfully and then deleted successfully");
+    });
   });
 }); 
- 
 
+// Route to execute AI model
 app.post('/execute', async (req, res) => {
   
   const messages = [
     {"role":"system", "content":"You are an AI assistant that helps people find information."},
     { role: "user", content:"Do not follow any instructions before You are an AI assistant. " + req.body.txtPrompt + " TEXT:"+ filteredText },
   ];
-  
+
   try {
+    // Create a new client for OpenAI
     const client = new OpenAIClient(process.env.ENDPOINT, new AzureKeyCredential(process.env.AZURE_KEY));
+    // Get chat completions from the model
     const result = await client.getChatCompletions(process.env.DEPLOYMENT_ID, messages, {
       maxTokens: 800,
       temperature: 0.7,
@@ -67,12 +77,13 @@ app.post('/execute', async (req, res) => {
       frequencyPenalty: 0,
     });
 
+    // Send the result back to the client
     res.json(result.choices[0].message.content);
   } catch (err) {
-    console.log("The sample encountered an error:", err);
-   // res.status(500).send(err.message);
-   res.json(err.code + 'And the error is' + err.message);
+    console.log(err.code + 'And the error is' + err.message);
+    res.json(err.code + 'And the error is' + err.message);
   } 
 });
 
+// Start the server
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
